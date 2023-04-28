439588bc0731846d5f9d379c783a14b7c3b054f7
romavsterg <romann.mikhaylov@gmail.com>
2023-04-28 16:16:57 +0300

    feat: added entering characters via the keyboard functional

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,16 +1,60 @@
 const body = document.querySelector(`body`)
 
+const checkKey = (key) => {
+    switch (key){
+        case 'Space':
+            input.value += ' '
+            break
+        case 'Tab':
+            input.value += '\t'
+            break
+        case 'CapsLock':
+            capsMode = !capsMode
+            document.querySelectorAll('.key').forEach((keyButton)=>{
+                if (keyButton.textContent == 'CapsLock') {
+                    keyButton.classList.toggle('capsMode')
+                }
+            })
+            console.log('caps lock:',capsMode)
+            document.querySelectorAll('.key').forEach((elem)=>{
+                if (!elem.classList.contains('wide-key')) {
+                    elem.textContent = capsMode? elem.textContent.toUpperCase() : elem.textContent.toLowerCase()
+                }
+            })
+            break
+        case 'Backspace':
+            let inputText = input.value.split('')
+            inputText.pop()
+            input.value = inputText.join('')
+            break
+        case 'ControlLeft':
+        case 'Win':
+        case 'AltLeft':
+        case 'AltRight':
+        case 'ControlRight':
+        case '←':
+        case '↓':
+        case '→':
+        case 'ShiftRight':
+        case 'ShiftLeft':
+        case 'Enter':
+            break
+        case 'Space':
+            input.value += ' '
+            break
+        default:
+            input.value += key
+            break
+    }
+}
 
-
-
-// const winEngKeys = [[['`', '`'], ['!', '1'], ['@', '2'], ['#', '3'], ['$', '4'], ['%', '5'], ['^', '6'], ['&', '7'], ['*', '8'], ['(', '9'], [')', '0'], ['_', '-'], ['+', '='], 'Backspace']]
 const winEngKeys = [['`',        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
                     ['Tab',      'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-                    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'k', 'l', "'"],
-                    ['Shift',    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '^', 'Shift'],
-                    ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '<', 'v', '>']]
+                    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', "'"],
+                    ['ShiftLeft',    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'ShiftRight'],
+                    ['ControlLeft', 'Win', 'AltLeft', 'Space', 'AltRight', 'ControlRight', '←', '↓', '→']]
 
-const winEngAuxiliaryKeys = ['Ctrl', 'Shift', 'CapsLock','CapsLock', 'Tab', 'Backspace', 'Win', 'Space', 'Alt', 'v', '<', '>', '^']
+const winEngAuxiliaryKeys = ['ControlLeft', 'Win', 'AltLeft', 'Space', 'AltRight', 'ControlRight', '←', '↓', '→','ShiftLeft','ShiftRight', 'Backspace', 'Tab','CapsLock']
 
 let capsMode = false
 
@@ -41,39 +85,29 @@ document.addEventListener('click', (target) => {
     if (target.target.classList.contains('key')) {
         let key = target.target.textContent
         console.log('pressed key', key)
-        switch (key){
-            case 'Space':
-                input.value += ' '
-                break
-            case 'Tab':
-                input.value += '\t'
-                break
-            case 'CapsLock':
-                capsMode = !capsMode
-                target.target.classList.toggle('capsMode')
-                console.log('caps lock:',capsMode)
-                document.querySelectorAll('.key').forEach((elem)=>{
-                    if (!elem.classList.contains('wide-key')) {
-                        elem.textContent = capsMode? elem.textContent.toUpperCase() : elem.textContent.toLowerCase()
-                    }
-                })
-                break
-            case 'Backspace':
-                let inputText = input.value.split('')
-                inputText.pop()
-                input.value = inputText.join('')
-                break
-            default:
-                input.value += key
-                break
-        }
+        checkKey(key)
     }
 })
 
 window.addEventListener('beforeunload', ()=>{localStorage.setItem('inputValue', input.value)})
 
-// document.addEventListener('keydown', (key)=>{
-//     console.log('pressed key', key)})
+document.addEventListener('keydown', (key)=>{
+    keyboard.querySelectorAll('.key').forEach((keyButton)=>{
+        if (keyButton.textContent == (key.key.replace(/\s/g, 'Space').length == 1 ? key.key : key.code)) {
+            keyButton.classList.add('pressed-key')
+            checkKey((key.key.replace(/\s/g, 'Space').length == 1 ? key.key : key.code))
+        }
+    })
+    
+})
+
+document.addEventListener('keyup', (key)=>{
+    keyboard.querySelectorAll('.key').forEach((keyButton)=>{
+        if (keyButton.textContent == (key.key.length == 1 ? key.key : key.code)) {
+            keyButton.classList.remove('pressed-key')
+        }
+    })
+})
 
 //     if (key.key === 'Tab') {
 //         input.textContent += '  '
